dd7096756332cd6b7df06107bbe9480e46fbba12
Matt Freels <matt@freels.name>
2008-09-25 12:18:08 -0700

    Rakefile for generating fakequery distribution files

--- jquery/src/intro.js
+++ jquery/src/intro.js
@@ -1,5 +1,3 @@
-// Map over jQuery in case of overwrite
-// Map over the $ in case of overwrite
 var _jQuery = jQuery, _$ = $;
 
 jQuery = $ = (function(){
---
+++ jquery/src/preamble.js
@@ -0,0 +1,7 @@
+/*
+ * fakeQuery, jQuery for Facebook
+ * jQuery Copyright (c) 2008 John Resig, http://jquery.com/
+ *
+ * fakeQuery modifications Copyright (c) 2008 Serious Business, http://seriousbusiness.com/
+ * 
+ */
